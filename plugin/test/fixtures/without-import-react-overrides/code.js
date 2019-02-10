41aafc531fc2f4f0919db5eda012b11cbb99ac24
Ilia Lesik <ilialesik@gmail.com>
2019-02-10 20:48:12 +0300

    Plugin: add test "without import react overrides"

---
+++ plugin/test/fixtures/without-import-react-overrides/code.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Comp = props => {
+    return (
+        <div>
+            <Comp1 {...o} arg={1} />
+        </div>
+    );
+};
---
+++ plugin/test/fixtures/without-import-react-overrides/output.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Comp = props => {
+  return <div>
+            <Comp1 {...o} arg={1} />
+        </div>;
+};
