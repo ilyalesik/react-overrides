33f0e7f96054d07a09f727fd92882443d9b2da94
Ilia Lesik <ilialesik@gmail.com>
2019-02-18 14:25:46 +0300

    Add return default empty object at index

--- src/index.js
+++ src/index.js
@@ -2,3 +2,8 @@
 
 export { getComponents } from "./getComponents";
 export type { ExtractOverridesProps } from "./ExtractOverridesProps";
+
+type TOObject = {||};
+const oObject: TOObject = Object.freeze({});
+
+export default oObject;
