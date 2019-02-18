e6ccb6496966e44a73b860704d0d9b51698038eb
Ilia Lesik <ilialesik@gmail.com>
2019-02-18 14:19:35 +0300

    Remove default object from index.js

--- src/index.js
+++ src/index.js
@@ -2,10 +2,3 @@
 
 export { getComponents } from "./getComponents";
 export type { ExtractOverridesProps } from "./ExtractOverridesProps";
-
-type TOObject = {
-    [string]: any
-};
-const oObject: TOObject = {};
-
-export default oObject;
